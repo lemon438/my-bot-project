2e3ab62b6437d2fe8f32eaf38bc68dab8a1240ca
lemon438 <lemongood.smart@gmail.com>
2024-07-22 13:39:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const { Permissions, ActivityType }
+const { Permissions, ActivityType } = require("discord.js");
 const client = new Discord.Client({ intents: ["Guilds"] })
 const keep_alive = require('./keep_alive.js')
 
